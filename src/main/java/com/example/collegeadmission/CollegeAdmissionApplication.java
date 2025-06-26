b7d11cf3004efbc484c19798e70e95a5e871fda2
Sourabh Singh <17sourav.singh@gmail.com>
2025-06-26 14:43:43 +0530

    Initial commit

---
+++ src/main/java/com/example/collegeadmission/CollegeAdmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegeadmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAdmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeAdmissionApplication.class, args);
+    }
+
+}
